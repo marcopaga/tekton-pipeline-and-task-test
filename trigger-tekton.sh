03ab622c9ee04df4824af04e939e9b2f84b4182e
Marco Paga <marcopaga@users.noreply.github.com>
2022-01-26 16:31:46 +0100

    Demo migrated to github pull request action

--- trigger-tekton.sh
+++ trigger-tekton.sh
@@ -1,6 +1,6 @@
 curl -v \
--H 'X-GitLab-Token: 1234567' \
--H 'X-Gitlab-Event: Push Hook' \
+-H 'X-GitHub-Event: pull_request' \
+-H 'X-Hub-Signature: sha1=11e4f57e845a7b5add651b2ae9a59eae5d8864b6' \
 -H 'Content-Type: application/json' \
---data-binary "@gitlab-push-event.json" \
+--data-binary "@github-push-event.json" \
 http://localhost:8081
